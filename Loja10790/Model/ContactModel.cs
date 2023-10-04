75a5f9f70e5318e02645c372656792e0b21c6f27
Pedro Bento <pbentoit@gmail.com>
2023-10-04 12:35:36 +0100

    Fixed bug and added .gitignore

--- Loja10790/Model/ContactModel.cs
+++ Loja10790/Model/ContactModel.cs
@@ -12,35 +12,35 @@ namespace Loja10790.Model
 
         [Column("email1")]
         [MaxLength(320)]
-        public string email1 { get; set; }
+        public string? email1 { get; set; }
 
         [Column("email2")]
         [MaxLength(320)]
-        public string email2 { get; set; }
+        public string? email2 { get; set; }
 
         [Column("phone1")]
         [MaxLength(15)]
-        public string phone1 { get; set; }
+        public string? phone1 { get; set; }
 
         [Column("phone2")]
         [MaxLength(15)]
-        public string phone2 { get; set; }
+        public string? phone2 { get; set; }
 
         [Column("address_line1")]
         [MaxLength(100)]
-        public string address_line1 { get; set; }
+        public string? address_line1 { get; set; }
 
         [Column("address_line2")]
         [MaxLength(100)]
-        public string address_line2 { get; set; }
+        public string? address_line2 { get; set; }
 
         [Column("city")]
         [MaxLength(50)]
-        public string city { get; set; }
+        public string? city { get; set; }
 
         [Column("postcode")]
         [MaxLength(10)]
-        public string postcode { get; set; }
+        public string? postcode { get; set; }
 
         [ForeignKey("country")]
         [Column("country")]
